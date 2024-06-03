ac28225d3e2d46a2ffd2c6f0bd755067d92a1941
Jim Flanagan <jimfl@squircle.local>
2024-06-03 14:19:35 -0700

    Make render() the only entry point to SVG

--- lib/tonic/svg.ex
+++ lib/tonic/svg.ex
@@ -21,9 +21,9 @@ defmodule Tonic.SVG do
     render(rest, [render_shape(shape) | tags])
   end
 
-  def render_shape(%Shape{children: []} = shape), do: render_leaf(shape)
+  defp render_shape(%Shape{children: []} = shape), do: render_leaf(shape)
 
-  def render_shape(%Shape{name: tag} = shape) do
+  defp render_shape(%Shape{name: tag} = shape) do
     attr = extract_attributes(shape) |> add_transforms(shape)
 
     [
@@ -38,24 +38,24 @@ defmodule Tonic.SVG do
     |> IO.iodata_to_binary()
   end
 
-  def render_leaf(%Shape{name: tag} = shape) do
+  defp render_leaf(%Shape{name: tag} = shape) do
     attr = extract_attributes(shape) |> add_transforms(shape)
     render_min_tag(tag, attr)
   end
 
-  def extract_attributes(%Shape{name: :rect} = shape) do
+  defp extract_attributes(%Shape{name: :rect} = shape) do
     [{x, y} | _] = shape.coords
-    [size | _] = shape.dimensions
-    shape.attributes |> Map.merge(%{x: x, y: y, width: size, height: size})
+    [width, height | _] = shape.dimensions
+    shape.attributes |> Map.merge(%{x: x, y: y, width: width, height: height})
   end
 
-  def extract_attributes(%Shape{name: :ellipse} = shape) do
+  defp extract_attributes(%Shape{name: :ellipse} = shape) do
     [{x, y} | _] = shape.coords
     [r_x, r_y | _] = shape.dimensions
     shape.attributes |> Map.merge(%{cx: x, cy: y, rx: r_x, ry: r_y})
   end
 
-  def extract_attributes(%Shape{name: :polyline} = shape) do
+  defp extract_attributes(%Shape{name: :polyline} = shape) do
     points =
       shape.coords
       |> Enum.map(fn {x, y} -> "#{x},#{y}" end)
@@ -64,7 +64,7 @@ defmodule Tonic.SVG do
     shape.attributes |> Map.put(:points, points)
   end
 
-  def extract_attributes(%Shape{name: :polygon} = shape) do
+  defp extract_attributes(%Shape{name: :polygon} = shape) do
     points =
       shape.coords
       |> Enum.map(fn {x, y} -> "#{x},#{y}" end)
@@ -73,56 +73,56 @@ defmodule Tonic.SVG do
     shape.attributes |> Map.put(:points, points)
   end
 
-  def extract_attributes(%Shape{attributes: attributes}), do: attributes
+  defp extract_attributes(%Shape{attributes: attributes}), do: attributes
 
-  def add_transforms(attributes, %Shape{transforms: []}), do: attributes
+  defp add_transforms(attributes, %Shape{transforms: []}), do: attributes
 
-  def add_transforms(attributes, shape) do
+  defp add_transforms(attributes, shape) do
     rendered = render_transform(shape.transforms, "") |> String.trim()
     attributes |> Map.put(:transform, rendered)
   end
 
-  def render_transform([], acc), do: acc
+  defp render_transform([], acc), do: acc
 
-  def render_transform([tfm | rest], acc) do
+  defp render_transform([tfm | rest], acc) do
     render_transform(rest, render_transform(tfm) <> acc)
   end
 
-  def render_transform(%Transform{type: :rotate, coords: nil} = tfm) do
+  defp render_transform(%Transform{type: :rotate, coords: nil} = tfm) do
     degrees = tfm.magnitude
     ~s/rotate(#{degrees}) /
   end
 
-  def render_transform(%Transform{type: :rotate} = tfm) do
+  defp render_transform(%Transform{type: :rotate} = tfm) do
     degrees = tfm.magnitude
     {x, y} = tfm.coords
     ~s/rotate(#{degrees} #{x} #{y}) /
   end
 
-  def render_transform(%Transform{type: :translate} = tfm) do
+  defp render_transform(%Transform{type: :translate} = tfm) do
     {x, y} = tfm.magnitude
     ~s/translate(#{x} #{y}) /
   end
 
-  def render_transform(%Transform{type: :scale} = tfm) do
+  defp render_transform(%Transform{type: :scale} = tfm) do
     {x, y} = tfm.magnitude
     ~s/scale(#{x} #{y}) /
   end
 
-  def render_transform(%Transform{type: :skew, magnitude: {x, 0}}) do
+  defp render_transform(%Transform{type: :skew, magnitude: {x, 0}}) do
     ~s/skewX(#{x}) /
   end
 
-  def render_transform(%Transform{type: :skew, magnitude: {0, y}}) do
+  defp render_transform(%Transform{type: :skew, magnitude: {0, y}}) do
     ~s/skewY(#{y}) /
   end
 
-  def render_transform(%Transform{type: :skew} = tfm) do
+  defp render_transform(%Transform{type: :skew} = tfm) do
     {x, y} = tfm.magnitude
     ~s/skewX(#{x}) skewY(#{y}) /
   end
 
-  def render_transform(%Transform{type: :matrix} = tfm) do
+  defp render_transform(%Transform{type: :matrix} = tfm) do
     {a, b, c, d, e, f} = tfm.magnitude
     ~s/matrix(#{a} #{b} #{c} #{d} #{e} #{f}) /
   end
