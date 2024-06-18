2df13a65a74d760d54ce8606611eed67a4d347b3
Jim Flanagan <jimfl@squircle.local>
2024-06-18 09:44:13 -0700

    More flexible canvas coordinate system

--- lib/tonic/canvas.ex
+++ lib/tonic/canvas.ex
@@ -1,13 +1,68 @@
 defmodule Tonic.Canvas do
   defstruct width: 0,
             height: 0,
-            options: [],
+            origin: {0, 0},
+            options: %{},
             shapes: [],
             grid_stack: [&Tonic.Grid.identity/1]
 
+  def new(options \\ []) do
+    %__MODULE__{} |> set_up(options)  
+  end
+
+  defp set_up(canvas, []), do: canvas
+
+  defp set_up(canvas, [{:origin, origin} | rest]) do
+    %{canvas | origin: origin } 
+    |> set_up(rest)
+  end
+
+  defp set_up(canvas, [{:height, h} | rest]) do
+    %{canvas | height: h} 
+    |> set_up(rest)
+  end
+
+  defp set_up(canvas, [{:width, w} | rest]) do
+    %{canvas | width: w} 
+    |> set_up(rest)
+  end
+
+  defp set_up(canvas, [{k, v} | rest]) do
+    %{canvas | options: canvas.options |> Map.put(k, v)} 
+    |> set_up(rest)
+  end
+
+  defp compute_view_box(canvas) do
+    {origin_x, origin_y} = canvas.origin
+
+    vb = 
+      {-origin_x, -origin_y, canvas.width - origin_x, canvas.height - origin_y}
+      |> Tuple.to_list()
+      |> Enum.join(" ")
+
+    %{canvas | options: canvas.options |> Map.put("view-box", vb)}
+  end
+
+  defp opts_to_attributes(canvas) do
+    for {k, v} <- canvas.options |> Map.to_list(), into: "" do
+      ~s( #{k}="#{v}")
+    end
+  end
+
+  def to_svg(canvas, {width, height}) do
+    %{canvas | 
+      options: canvas.options 
+        |> Map.put(:height, height) 
+        |> Map.put(:width, width)
+    }
+    |> to_svg()
+  end
+
   def to_svg(canvas) do
-    ~s(<svg xmlns="http://www.w3.org/2000/svg" width="#{canvas.width}" height="#{canvas.height}">) <>
+    canvas = canvas |> compute_view_box()
+    ~s(<svg xmlns="http://www.w3.org/2000/svg" #{canvas |> opts_to_attributes()}>) <>
       (canvas.shapes |> Tonic.SVG.render()) <>
       ~s(</svg>)
   end
+
 end
---
+++ test/tonic/canvas_test.exs
@@ -0,0 +1,30 @@
+defmodule Tonic.CanvasTest do
+
+	use ExUnit.Case
+
+	alias Tonic.Canvas
+
+	test "canvas with default origin" do
+    canvas = Canvas.new(height: 50, width: 50)
+		%{height: 50, width: 50, origin: {0, 0}} = canvas
+		svg = canvas |> Canvas.to_svg()
+		assert svg =~  ~r(view-box="0 0 50 50") 
+	end
+	
+	test "canvas with shifted origin" do
+    canvas = Canvas.new(height: 50, width: 50, origin: {25, 25})
+		%{height: 50, width: 50, origin: {25, 25}} = canvas
+		svg = canvas |> Canvas.to_svg()
+		assert svg =~  ~r(view-box="-25 -25 25 25") 
+	end
+
+	test "canvas with shifted origin, resized" do
+    canvas = Canvas.new(height: 50, width: 50, origin: {25, 25})
+		%{height: 50, width: 50, origin: {25, 25}} = canvas
+		svg = canvas |> Canvas.to_svg({100, 250})
+		assert svg =~  ~r(view-box="-25 -25 25 25") 
+		assert svg =~ ~r(height="250")
+		assert svg =~ ~r(width="100")
+	end
+
+end
